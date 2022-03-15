5726649e709f9c2a6ae1ac129d3feaa8594ea79b
Dmitriy Safonov <d.lexed@gmail.com>
2022-03-15 13:13:50 +0300

    add lesson homework 18

--- src/main/java/lesson_18/Main.java
+++ src/main/java/lesson_18/Main.java
@@ -1,4 +1,18 @@
 package lesson_18;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /*
 1. Добавить поддержку Maven для проекта
 
@@ -6,7 +20,36 @@ package lesson_18;
  или наполнить отдельным раннером). Необходимо в  отдельном потоке считывать его содержимое, перекладывая
  каждую строку в отдельный файл (количество создаваемых  файлов соответствует количеству строк в файле).
 
-3. Рекомендую начинать думать о проекте (в части темы для  разработки)
  */
 public class Main {
+    private static final String PATH_FILE = "/newfile.txt";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("считываем текстовый файл");
+        try (BufferedReader bufferedReader = new BufferedReader
+                (new InputStreamReader(Main.class.getResourceAsStream(PATH_FILE), StandardCharsets.UTF_8))) {
+            final String extension = FilenameUtils.getExtension(PATH_FILE);
+            AtomicInteger index = new AtomicInteger();
+            bufferedReader.lines().filter(StringUtils::isNotBlank)
+                    .forEach(line -> writeToNewFile(extension, index, line)
+            );
+        }
+    }
+
+    private static void writeToNewFile(String extension, AtomicInteger index, String line) {
+        try {
+            Path path = Paths.get("C:\\out", "result_" + (index.incrementAndGet()) + "." + extension);
+            System.out.printf("Начало записи в файл %s\n", path.getFileName());
+            Path pathParent = path.getParent();
+          if(!Files.exists(pathParent)){
+              if(!pathParent.toFile().mkdir()){
+                  System.out.println("Ошибка при создание папки");
+                  return;
+              }
+          }
+            Files.writeString(path, line, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
